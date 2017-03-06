2621d2591d3da994565307e1156662a63ba3465e
Shelley Vohr <csvohr1995@gmail.com>
2017-03-05 21:36:37 -0500

    Fix some errors

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   community: process.env.COMMUNITY_NAME || 'SF Bay Interns 2017',
   slackUrl: process.env.SLACK_URL || 'sfbayinterns2017.slack.com',
-  slacktoken: process.env.SLACK_TOKEN || 'xoxp-121119456850-121119456946-149826845410-ad73a713bfa8556fe7bffb7b1c6fa050',
+  slacktoken: process.env.SLACK_TOKEN || 'xoxp-121119456850-121119456946-150026735780-13568c4b96951851bb005fec3b12bdbd',
   locale: process.env.LOCALE || "en",
 };

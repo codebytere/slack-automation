5af1d4f0849ec667a5f98612252280072276523f
Shelley Vohr <csvohr1995@gmail.com>
2017-03-05 21:16:19 -0500

    Add initial fileset

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var express = require('express'),
+const express = require('express'),
     path = require('path'),
     favicon = require('serve-favicon'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     config = require('./config'),
-    i18n = require("i18n");
+    i18n = require('i18n');
 
-var routes = require('./routes/index');
+const routes = require('./routes/index');
 
-var app = express();
+const app = express();
 
 i18n.configure({
-    defaultLocale: "en",
+    defaultLocale: 'en',
     directory: __dirname + '/locales',
-    autoReload: true
+    autoReload: true,
 });
 
 i18n.setLocale(config.locale);
@@ -22,50 +22,39 @@ i18n.setLocale(config.locale);
 // default: using 'accept-language' header to guess language settings
 app.use(i18n.init);
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: err
+            error: err,
         });
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {}
+        error: {},
     });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,79 +1,67 @@
-var express = require('express');
-var router = express.Router();
-var request = require('request');
-var config = require('../config');
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const config = require('../config');
 
 router.get('/', function(req, res) {
-  res.setLocale(config.locale);
-  res.render('index', { community: config.community,
-                        tokenRequired: !!config.inviteToken });
+    res.setLocale(config.locale);
+    res.render('index', {community: config.community});
 });
 
 router.post('/invite', function(req, res) {
-  if (req.body.email && (!config.inviteToken || (!!config.inviteToken && req.body.token === config.inviteToken))) {
-    request.post({
-        url: 'https://'+ config.slackUrl + '/api/users.admin.invite',
-        form: {
-          email: req.body.email,
-          token: config.slacktoken,
-          set_active: true
+    if (req.body.email) {
+        request.post({
+            url: 'https://' + config.slackUrl + '/api/users.admin.invite',
+            form: {
+                email: req.body.email,
+                name: req.body.name,
+                token: config.slacktoken,
+                set_active: true,
+            },
+        }, function(err, httpResponse, body) {
+            if (err) {
+                return res.send('Error:' + err);
+            }
+            const parsed_body = JSON.parse(body);
+            if (parsed_body.ok) {
+                res.render('result', {
+                    community: config.community,
+                    message: 'Success! Check &ldquo;' + req.body.email + '&rdquo; for an invite from Slack.',
+                });
+            } else {
+                let error = parsed_body.error;
+                if (error === 'already_invited' || error === 'already_in_team') {
+                    res.render('result', {
+                        community: config.community,
+                        message: 'Success! You were already invited.<br>' +
+                       'Visit <a href="https://' + config.slackUrl + '">' + config.community + '</a>',
+                    });
+                    return '';
+                } else if (error === 'invalid_email') {
+                    error = 'The email you entered is an invalid email.';
+                } else if (error === 'invalid_auth') {
+                    error = 'Something has gone wrong. Please contact a system administrator.';
+                }
+
+                res.render('result', {
+                    community: config.community,
+                    message: 'Failed! ' + error,
+                    isFailed: true,
+                });
+            }
+        });
+    } else {
+        const errMsg = [];
+        if (!req.body.email) {
+            errMsg.push('your email is required');
         }
-      }, function(err, httpResponse, body) {
-        // body looks like:
-        //   {"ok":true}
-        //       or
-        //   {"ok":false,"error":"already_invited"}
-        if (err) { return res.send('Error:' + err); }
-        body = JSON.parse(body);
-        if (body.ok) {
-          res.render('result', {
+
+        res.render('result', {
             community: config.community,
-            message: 'Success! Check &ldquo;'+ req.body.email +'&rdquo; for an invite from Slack.'
-          });
-        } else {
-          var error = body.error;
-          if (error === 'already_invited' || error === 'already_in_team') {
-            res.render('result', {
-              community: config.community,
-              message: 'Success! You were already invited.<br>' +
-                       'Visit <a href="https://'+ config.slackUrl +'">'+ config.community +'</a>'
-            });
-            return;
-          } else if (error === 'invalid_email') {
-            error = 'The email you entered is an invalid email.';
-          } else if (error === 'invalid_auth') {
-            error = 'Something has gone wrong. Please contact a system administrator.';
-          }
-
-          res.render('result', {
-            community: config.community,
-            message: 'Failed! ' + error,
-            isFailed: true
-          });
-        }
-      });
-  } else {
-    var errMsg = [];
-    if (!req.body.email) {
-      errMsg.push('your email is required');
+            message: 'Failed! ' + errMsg.join(' and ') + '.',
+            isFailed: true,
+        });
     }
-
-    if (!!config.inviteToken) {
-      if (!req.body.token) {
-        errMsg.push('valid token is required');
-      }
-
-      if (req.body.token && req.body.token !== config.inviteToken) {
-        errMsg.push('the token you entered is wrong');
-      }
-    }
-
-    res.render('result', {
-      community: config.community,
-      message: 'Failed! ' + errMsg.join(' and ') + '.',
-      isFailed: true
-    });
-  }
 });
 
 module.exports = router;

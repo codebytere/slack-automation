2c32fbdce488b1b64d378115e6649073cd0c6772
Shelley Vohr <csvohr1995@gmail.com>
2017-03-05 21:38:07 -0500

    Remove tester token

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   community: process.env.COMMUNITY_NAME || 'SF Bay Interns 2017',
   slackUrl: process.env.SLACK_URL || 'sfbayinterns2017.slack.com',
-  slacktoken: process.env.SLACK_TOKEN || 'xoxp-121119456850-121119456946-150026735780-13568c4b96951851bb005fec3b12bdbd',
+  slacktoken: process.env.SLACK_TOKEN || 'YOUR SLACK TOKEN',
   locale: process.env.LOCALE || "en",
 };
